4166d03bf0f07cc79121cb57eac3ff3558f6fb4a
Theo Caldas <theocaldas28@gmail.com>
2026-04-29 15:31:52 -0300

    fix(circuit): compile script

--- compile.circuit.sh
+++ compile.circuit.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-DIR=circuits/dist_step.circom
-circom $DIR -o artifacts --r1cs --sym --wasm --prime vesta
+set -e
+
+if [ -z "$1" ]; then
+    echo "Fail! Usage: $0 {CIRCUIT_PATH} {OUTPUT_PATH:-artifacts}"
+    exit 1
+fi
+
+OUTPUT_PATH=artifacts
+CIRCUIT_PATH=$1
+
+if [ -n "$2" ]; then
+    OUTPUT_PATH=$2
+fi
+
+if [ ! -d "$OUTPUT_PATH" ]; then
+    mkdir $OUTPUT_PATH
+fi
+
+circom $CIRCUIT_PATH -o $OUTPUT_PATH --r1cs --sym --wasm --prime vesta
